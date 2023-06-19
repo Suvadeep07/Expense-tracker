1cb9412532ba9fc0bbdeb6829051bd8ed95e982b
Suvadeep Mukherjee <mukherjee07.suvadeep@gmail.com>
2023-06-19 17:01:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+let form = document.getElementById("addForm");
+let itemList = document.getElementById("items");
+let filter = document.getElementById("filter");
+
+let addition = (e) => {
+  e.preventDefault();
+
+  let newItem = document.getElementById("item").value;
+  let li = document.createElement("li");
+  li.className = "list-group-item";
+  li.appendChild(document.createTextNode(newItem));
+
+  let desc = document.getElementById("desc").value;
+  li.appendChild(document.createTextNode(" " + desc));
+
+  let deleteBtn = document.createElement("button");
+  deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+  deleteBtn.appendChild(document.createTextNode("X"));
+
+  let editBtn = document.createElement("button");
+  editBtn.className = "btn btn-sm float-right";
+  editBtn.appendChild(document.createTextNode("Edit"));
+
+  li.appendChild(editBtn);
+
+  li.appendChild(deleteBtn);
+
+  itemList.appendChild(li);
+};
+form.addEventListener("submit", addition);
+
+let removeItem = (e) => {
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are you sure ?")) {
+      let li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+};
+
+itemList.addEventListener("click", removeItem);
+
+let filterItems = (e) => {
+  let text = e.target.value.toLowerCase();
+  let items = itemList.getElementsByTagName("li");
+
+  Array.from(items).forEach((item) => {
+    let itemName = item.firstChild.textContent;
+    let descName = item.childNodes[1].textContent;
+    if (
+      itemName.toLowerCase().indexOf(text) != -1 ||
+      descName.toLowerCase().indexOf(text) != -1
+    ) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+};
+
+filter.addEventListener("keyup", filterItems);
